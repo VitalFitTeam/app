8bb141c0cab2778780d7e816b6a5d21f8df5d366
MrWisz <eduardoluis09@hotmail.com>
2025-10-01 19:02:06 -0400

    Probando Husky

---
+++ VitalFit/.prettierrc.js
@@ -0,0 +1,15 @@
+// .prettierrc.js
+/** @type {import("prettier").Config} */
+module.exports = {
+	arrowParens: 'always',
+	bracketSpacing: true,
+	bracketSameLine: true,
+	jsxSingleQuote: true,
+	singleQuote: true,
+	semi: true,
+	printWidth: 100,
+	useTabs: true,
+	tabWidth: 4,
+	endOfLine: 'lf',
+	trailingComma: 'all',
+};
