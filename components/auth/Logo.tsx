3069f85ff8b301ab2a07b65a49a9137eb1d55895
joseA <nobujose6@gmail.com>
2025-10-04 11:24:50 -0400

    imagen

--- components/auth/Logo.tsx
+++ components/auth/Logo.tsx
@@ -5,7 +5,7 @@ export function Logo() {
 	return (
 		<Image
 			source={require('@/assets/images/logovitalfit.png')}
-			style={{ width: 60, height: 60, marginBottom: 24 }}
+			style={{ width: 200, height: 200, marginBottom: 24 }}
 			contentFit='contain'
 		/>
 	);
