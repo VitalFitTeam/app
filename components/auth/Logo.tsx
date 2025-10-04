8a88d2465a1f629cb5c51d49961961f116b6314d
joseA <nobujose6@gmail.com>
2025-10-04 09:05:17 -0400

    cambi imagen logo

--- components/auth/Logo.tsx
+++ components/auth/Logo.tsx
@@ -4,9 +4,9 @@ import { Image } from 'expo-image';
 export function Logo() {
 	return (
 		<Image
-			source={require('@/assets/images/isotipo.png')}
+			source={require('@/assets/images/logovitalfit.png')}
 			style={{ width: 60, height: 60, marginBottom: 24 }}
-			contentFit="contain"
+			contentFit='contain'
 		/>
 	);
 }
