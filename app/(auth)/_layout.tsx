87099cf041764cf63cdbd91d34f08c96ec22cc97
MrWisz <eduardoluis09@hotmail.com>
2025-10-14 23:40:18 -0400

    cambiando el app.config.json

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -5,7 +5,6 @@ import { Stack } from 'expo-router';
 export default function AuthLayout() {
 	return (
 		<Stack>
-			<Stack.Screen name='home' options={{ headerShown: false }} />
 			<Stack.Screen name='login' options={{ headerShown: false }} />
 			<Stack.Screen name='register' options={{ headerShown: false }} />
 			<Stack.Screen name='forgot-password' options={{ title: 'Recuperar Contraseña' }} />
