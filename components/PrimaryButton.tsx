f64c42a770a4410c03d498b52f42b1ba98f21103
Frank <138620399+Frank1360@users.noreply.github.com>
2025-10-17 21:06:02 -0400

    forgot

--- components/PrimaryButton.tsx
+++ components/PrimaryButton.tsx
@@ -7,12 +7,11 @@ import { Text, TouchableOpacity, type TouchableOpacityProps } from 'react-native
 interface Props extends TouchableOpacityProps {
 	title: string;
 }
-
-export function PrimaryButton({ title, ...props }: Props) {
+export function PrimaryButton({ title, style, ...props }: Props) {
 	return (
 		<TouchableOpacity
 			className='h-12 w-full items-center justify-center rounded-md'
-			style={{ backgroundColor: Colors.light.tint }} // Usamos el color Naranja Vital del tema
+			style={[{ backgroundColor: Colors.light.tint }, style]}
 			{...props}>
 			<Text className='text-white text-base font-bold'>{title}</Text>
 		</TouchableOpacity>
