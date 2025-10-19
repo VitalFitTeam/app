23130fa322b4be9cbb1f9af572066c7572ad89e6
MrWisz <eduardoluis09@hotmail.com>
2025-10-18 20:54:13 -0400

    nombre del dashboard conectado a la api

--- app/(tabs)/dashboard.tsx
+++ app/(tabs)/dashboard.tsx
@@ -5,14 +5,67 @@ import { TodayRoutineCard } from '@/components/auth/dashboard/todayroutinecard';
 import { UserHeader } from '@/components/auth/dashboard/userheader';
 import { WeekCalendar } from '@/components/auth/dashboard/weekcalendar';
 import { ThemedView } from '@/components/themed-view';
-import { ScrollView } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, ScrollView } from 'react-native';
+
+const API_URL = process.env.EXPO_PUBLIC_API_URL;
 
 export default function DashboardScreen() {
+	const [firstName, setFirstName] = useState<string | null>(null);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		const fetchUser = async () => {
+			try {
+				const token = await AsyncStorage.getItem('token');
+
+				if (!token) {
+					console.error('No se encontró token en AsyncStorage');
+					return;
+				}
+
+				const response = await fetch(`${API_URL}/user/whoami`, {
+					method: 'GET',
+					headers: {
+						Accept: 'application/json',
+						'Content-Type': 'application/json',
+						Authorization: `Bearer ${token}`,
+					},
+				});
+
+				const text = await response.text();
+
+				if (!response.ok) {
+					console.error('Error al obtener el usuario:', response.status);
+					return;
+				}
+
+				const data = JSON.parse(text);
+				setFirstName(data?.user?.first_name || 'Usuario');
+			} catch (error) {
+				console.error('Error en la solicitud:', error);
+			} finally {
+				setLoading(false);
+			}
+		};
+
+		fetchUser();
+	}, []);
+
+	if (loading) {
+		return (
+			<ThemedView className='flex-1 justify-center items-center bg-white dark:bg-neutral-950'>
+				<ActivityIndicator size='large' color='#F27F2A' />
+			</ThemedView>
+		);
+	}
+
 	return (
 		<ThemedView className='flex-1 bg-white dark:bg-neutral-950 px-4 pt-10'>
 			<ScrollView showsVerticalScrollIndicator={false}>
 				<UserHeader
-					name='Albani'
+					name={firstName ?? 'Usuario'}
 					message='Es hora de desafiar tus límites'
 					avatarUrl='https://randomuser.me/api/portraits/women/45.jpg'
 				/>
