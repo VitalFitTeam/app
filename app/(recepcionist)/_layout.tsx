14e1da0d27ba53547a4daecbcdad093327a02767
dsanchez1998 <danielsanchez192@gmail.com>
2025-11-30 10:42:48 -0400

    Se mejoro la función que hacer que puedas salir de la app, y que no genere choques con las otras pantallas

--- app/(recepcionist)/_layout.tsx
+++ app/(recepcionist)/_layout.tsx
@@ -1,6 +1,11 @@
 import RecepcionistNavBar from '@/components/recepcionista/RecepcionistNavBar';
+import { BackHandlerProvider } from '@/contexts/BackHandlerContext';
 import React from 'react';
 
 export default function RecepcionistLayout() {
-  return <RecepcionistNavBar />;
+  return (
+    <BackHandlerProvider>
+      <RecepcionistNavBar />
+    </BackHandlerProvider>
+  );
 }
